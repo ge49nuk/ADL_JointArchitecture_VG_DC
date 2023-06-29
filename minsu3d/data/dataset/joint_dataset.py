Fixed F1 scores calculation

minsu3d/data/dataset/joint_dataset.py
@@ -13,8 +13,8 @@ class JointDataset(Dataset):
         self.split = split
         # self.max_num_point = cfg.data.max_num_point
         self.instance_size = cfg.data.instance_size
-        self.augs_per_scene = 4
-        self.num_descrs = 12
+        self.augs_per_scene = 1
+        self.num_descrs = 4
         self._load_from_disk()
 
     def _load_from_disk(self):
@@ -42,8 +42,9 @@ class JointDataset(Dataset):
                 descr_folder = os.path.join(scan_folder, "descr")
                 descr_fns = os.listdir(descr_folder)
                 num_descrs = min(self.num_descrs, len(descr_fns))
-                selected_idx = np.random.choice(len(descr_fns) , num_descrs, replace=False)
-                for i in selected_idx:
+                # selected_idx = np.random.choice(len(descr_fns) , num_descrs, replace=False)
+                # for i in selected_idx:
+                for i in range(num_descrs):
                     descr_fn = descr_fns[i]
                     descr_file = os.path.join(descr_folder, descr_fn)
                     self.descrs.append(torch.load(descr_file))
@@ -81,5 +82,10 @@ class JointDataset(Dataset):
         data["queried_objs"] = np.array(descr['queried_objs'])
         data["instance_ids"] = scan['instance_ids']
         data["scan_desc_id"] = descr['scan_desc_id']
+
+        # For corpus generation
+        data["scene_id"] = descr['scene_id']
+        data["object_id"] = descr['object_id']
+        data["object_name"] = descr['object_name']
 
         return data

minsu3d/data/joint_data_module.py
@@ -83,6 +83,10 @@ def _collate_fn(batch):
     instance_ids = []
     scan_desc_id = []
 
+    scene_ids = []
+    object_ids = []
+    object_names = []
+
     for b in batch:
         point_features = b["point_features"]
         insts = np.split(point_features, b["instance_splits"], axis=0) # (#instances, #pts, dim_pt_feats)
@@ -96,12 +100,17 @@ def _collate_fn(batch):
         target_word_ids.append(torch.from_numpy(b["target_word_ids"]))
         num_tokens.append(b["num_tokens"])
         target_classes.append(torch.from_numpy(b["target_class"]))
+        ious_on_cluster.append(b["ious_on_cluster"])
         
         queried_objs.append(b["queried_objs"])
         proposals_idx.append(torch.from_numpy(b["proposals_idx"]))
         instance_ids.append(torch.from_numpy(b["instance_ids"]))
         scan_desc_id.append(b["scan_desc_id"])
-        ious_on_cluster.append(b["ious_on_cluster"])
+
+        scene_ids.append(b["scene_id"])
+        object_ids.append(b["object_id"])
+        object_names.append(b["object_name"])
+        
     
     data = {'instances': torch.cat(instances, dim=0)}
     data['scene_splits'] = scene_splits # need to extract [1:-1]
@@ -117,5 +126,9 @@ def _collate_fn(batch):
     data['proposals_idx'] = proposals_idx
     data['instance_ids'] = instance_ids
     data['scan_desc_id'] = scan_desc_id
+
+    data['scene_ids'] = scene_ids
+    data['object_ids'] = object_ids
+    data['object_names'] = object_names
 
     return data

minsu3d/model/joint_preprocess.py
@@ -5,7 +5,7 @@ import hydra
 import os
 import pytorch_lightning as pl
 from minsu3d.common_ops.functions import common_ops
-from transformers import BertConfig, BertModel
+from transformers import BertModel
 from minsu3d.model.softgroup import SoftGroup
 import json
 import pickle
@@ -36,14 +36,14 @@ class JointPreprocessModelT(pl.LightningModule):
         self.split = 'train'
         self.scan_list = []
 
-        self.min_inst_size = 1000000
-        self.max_inst_size = 0
     
     def configure_optimizers(self):
         return hydra.utils.instantiate(self.hparams.cfg.model.optimizer, params=self.parameters())
 
     def forward(self, data_dict):
         scan_id = data_dict["scan_ids"][0]
+        object_id = data_dict["object_ids"][0]
+        object_name = data_dict["object_names"][0]
         
         # SoftGroup
         self.softgroup.eval()
@@ -54,17 +54,6 @@ class JointPreprocessModelT(pl.LightningModule):
         point_features = (output_dict["point_features"])[proposals_idx[:, 1]]
 
         _, counts = proposals_idx[:, 0].unique(return_counts=True)
-
-        min_count = counts.min()
-        max_count = counts.max()
-        if min_count < self.min_inst_size:
-            self.min_inst_size = min_count
-            with open(f"min_inst_size_{self.split}.txt", 'w') as f:
-                print(min_count, file=f)
-        if max_count > self.max_inst_size:
-            self.max_inst_size = max_count
-            with open(f"max_inst_size_{self.split}.txt", 'w') as f:
-                print(max_count, file=f)
 
         for i, count in enumerate(counts[:-1]):
             counts[i+1] += count
@@ -88,8 +77,7 @@ class JointPreprocessModelT(pl.LightningModule):
         num_tokens = data_dict["num_descr_tokens"][0]
         
         # Mapping for descrption object: object name -> class id
-        obj_name = data_dict["object_names"][0]
-        obj_name = np.repeat(obj_name, len(class_names))
+        obj_name = np.repeat(object_name, len(class_names))
         target_class = torch.tensor(class_names == obj_name).float() # One-hot vector
 
         # Get descr_id for naming the file
@@ -112,7 +100,7 @@ class JointPreprocessModelT(pl.LightningModule):
                         'proposals_idx': output_dict["proposals_idx"].cpu().numpy(), 'instance_ids': data_dict["instance_ids"].cpu().numpy(),
                         'ious_on_cluster': ious_on_cluster.cpu().numpy()}, scan_file)
             self.scan_list.append(aug_id)
-        torch.save({'queried_objs': queried_objs, 'text_embedding': text_embedding.cpu().numpy(), 'target_word_ids': target_word_ids.cpu().numpy(), 
+        torch.save({'scene_id': scan_id, 'object_id': object_id, 'object_name': object_name, 'queried_objs': queried_objs, 'text_embedding': text_embedding.cpu().numpy(), 'target_word_ids': target_word_ids.cpu().numpy(), 
                     'num_tokens': num_tokens, 'target_class': target_class.cpu().numpy(), 'scan_desc_id': scan_desc_id}, descr_file)
         
         # with open(full_path, 'wb') as fp:

minsu3d/model/transformer_light.py
@@ -117,7 +117,6 @@ class Transformer_Light(nn.Module):
         CLS_loss = 0.0
         DC_loss = 0.0
         candidate_descr_list = []
-        gt_descr_list = []
         for i, scene in enumerate(scenes):
             num_proposals = num_instances[i]
             len_text_tokens = num_tokens[i] - 1
@@ -170,15 +169,12 @@ class Transformer_Light(nn.Module):
                 assert DC_scores.size() == (len_text_tokens, self.size_vocab)
                 # For calculating DC scores
                 predicted_ids = DC_scores.argmax(dim=-1)
-                predicted_words = tokenizer.convert_ids_to_tokens(predicted_ids)[:-1] # Discard [SEP]
-                predicted_str = " ".join(predicted_words)
+                predicted_words = tokenizer.convert_ids_to_tokens(predicted_ids)
+                predicted_str = tokenizer.convert_tokens_to_string(predicted_words)
+                predicted_str = predicted_str.replace('[SEP]', '.')
+                predicted_str = predicted_str.replace(' \' s ', ' \'s ')
                 candidate_descr = "sos " + predicted_str + " eos"
                 candidate_descr_list.append(candidate_descr)
-                gt_wd_ids = target_word_ids[i][1:len_text_tokens] # Discard [CLS] [SEP]
-                gt_words = tokenizer.convert_ids_to_tokens(gt_wd_ids)
-                gt_str = " ".join(gt_words)
-                gt_descr = "sos " + gt_str + " eos"
-                gt_descr_list.append(gt_descr)
                 # Compute DC loss
                 DC_loss += self.loss_criterion_DC(DC_scores, target_word_ids[i][1:(len_text_tokens+1)])
         
@@ -189,7 +185,7 @@ class Transformer_Light(nn.Module):
         return {"Match_scores": Match_scores_list, "Match_loss": Match_loss,
                 "CLS_scores": CLS_scores_list, "CLS_loss": CLS_loss,
                 "DC_scores": DC_scores_list, "DC_loss": DC_loss,
-                "candidate_descrs": candidate_descr_list, "gt_descrs": gt_descr_list}
+                "candidate_descrs": candidate_descr_list}
     
 
     def inferrence_DC(self, data_dict):