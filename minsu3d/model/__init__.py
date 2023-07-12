f13229d3beb96eba4575d6bdbe77ee63511b9ef9
zhhliang <z.liang@tum.com>
2023-07-12 22:35:42 +0200

    Deleted unnecessary files

--- minsu3d/model/__init__.py
+++ minsu3d/model/__init__.py
@@ -1,6 +1,3 @@
-from .pointgroup import PointGroup
 from .softgroup import SoftGroup
-from .joint_arch import Joint_Arch
 from .joint_light import Joint_Light
 from .joint_preprocess import JointPreprocessModelT, JointPreprocessModelV
-from .hais import HAIS
